c5e1cafb4046bef058f1db0a57db9a36ee08a8ee
Roxana Olimpia <olimpiaroxana@yahoo.com>
2017-06-07 22:28:04 +0300

    comentariu

--- src/main/java/ro/sci/rentacar/Main.java
+++ src/main/java/ro/sci/rentacar/Main.java
@@ -6,6 +6,6 @@ package ro.sci.rentacar;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println();
+        System.out.println;
     }
 }
---
+++ src/main/java/ro/sci/rentacar/util/FuelType.java
@@ -0,0 +1,7 @@
+package ro.sci.rentacar.util;
+
+/**
+ * Created by Roxana on 6/7/2017.
+ */
+public class FuelType {
+}
