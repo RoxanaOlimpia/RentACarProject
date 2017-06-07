365d16f0f1df8532bdfb90e8e34859140b6da335
Roxana Olimpia <olimpiaroxana@yahoo.com>
2017-06-07 22:19:02 +0300

    initial commit

---
+++ src/main/java/ro/sci/rentacar/Main.java
@@ -0,0 +1,11 @@
+package ro.sci.rentacar;
+
+/**
+ * Created by Roxana on 6/7/2017.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println();
+    }
+}
